a1ae8aaddbb60473aa854296a0ed87f2853c6b04
Kyle Graden <kylegraden3@gmail.com>
2019-02-12 19:01:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = { prop: '1'}
+const obj = { prop: '1'}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var obj = { prop: '1'}
+  return Object.assign({}, obj, { ['prop2']: "2" })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  obj['prop2'] = '2'
+  return obj
+}
+
+function deleteFromObjectByKey(object, key){
+var obj = { prop: '1'}
+var newObj = Object.assign({}, obj)
+delete newobj.prop
+return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  var obj = { prop: '1'}
+  delete obj.prop
+  return obj
+}
